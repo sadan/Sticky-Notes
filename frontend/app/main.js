1667901acabd431d1203045f2f4704415b259113
Sadan Arshad <sadanarshad@gmail.com>
2016-07-22 16:07:08 +0500

    - fixed frontend structure

---
+++ frontend/app/main.js
@@ -0,0 +1,5 @@
+"use strict";
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var home_component_1 = require('./home/home.component');
+platform_browser_dynamic_1.bootstrap(home_component_1.HomeComponent, []);
+//# sourceMappingURL=main.js.map
---
+++ frontend/app/main.ts
@@ -0,0 +1,6 @@
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { HomeComponent } from './home/home.component';
+
+bootstrap(HomeComponent, [
+
+]);
